8df1411362859f2f9748493b6081f7cd1fccb359
Karina Basurto <kar.basurto@hotmail.com>
2023-05-31 16:08:12 +0100

    add seeds and controller restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,12 @@
 class Restaurant < ApplicationRecord
 
-  validate :name, precense: true
-  validate :name, precense: true
-  validate :name, precense: true
-  validate :name, precense: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], message: "%{value} is not included in the list" }
+  has_many :reviews, dependent: :destroy
+  # blongs_to :reviews,
+  # validates :ratings, presence: true
+  
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }
+  # validates :rating, length: { in: 0..5 }
+  # validates :rating, length: {maximum: 5}
+  validates :rating, :inclusion => { :in => 0..5 }, :presence => { :message => " must be within 0-5" }
 end
