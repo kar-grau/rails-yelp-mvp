665b3ad4faddf30ceb63cea08b4ff211c50695ca
Karina Basurto <kar.basurto@hotmail.com>
2023-05-31 21:37:19 +0100

    restaurant controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "restaurants#index"
-  resources :restaurants
- 
 
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
